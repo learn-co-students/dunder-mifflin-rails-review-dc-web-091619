5711cf019d153c664e30a5e65483978a8a63a7ef
github username <github email address>
2019-10-17 16:26:19 -0400

    done

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,54 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create 
+    @employee = Employee.new(employee_params)
+    if @employee.valid?
+      @employee.save
+      redirect_to employee_path(@employee)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.find(params[:id])
+    @employee.update(employee_params)
+
+    redirect_to employee_path(@employee)
+  end
+
+  def destroy
+    Employee.destroy(params[:id])
+    redirect_to employees_path
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(
+      :first_name,
+      :last_name,
+      :alias,
+      :title,
+      :office,
+      :dog_id
+    )
+  end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :alias, presence: true, uniqueness: {case_sensitive: false}
+    validates :title, presence: true, uniqueness: {case_sensitive: false}
+
+    def full_name
+        # first_name + " " + last_name
+        "#{first_name} #{last_name}"
+    end
 end
