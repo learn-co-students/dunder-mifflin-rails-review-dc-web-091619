ab3ae3e698a494f4c6b636f8542f15d5687b0c44
vidhisharma3193 <vidhisharma3193@gmail.com>
2021-05-20 16:13:03 -0500

    fix routes for solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
+  resources :dogs, only: [:index, :show]
+  resources :employees, only: [:index, :show, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
